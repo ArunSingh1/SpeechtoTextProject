de30605f62d6263cd5e347bab76e0205a39f908e
ArunSingh1 <arunsinghmailid@gmail.com>
2023-08-11 21:35:34 +0200

    react app created buttons and timer created

---
+++ speechtotextui/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Speechtotext from './Components/Speechtotext';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Speechtotext></Speechtotext>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
